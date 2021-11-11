3b2ccabcf8992e2afdf05ef4f2c666c9e22158cf
MadmouseKed <75257164+MadmouseKed@users.noreply.github.com>
2021-11-11 23:14:10 +0100

    Final ui version.

--- Application/Main.py
+++ Application/Main.py
@@ -6,26 +6,70 @@ Created on Wed Nov  3 10:38:36 2021
 """
 
 from tkinter import *
-from dataProcessing import prediction, load_joblib
+from dataProcessing import prediction, loadJobLib
+import math
+import numpy as np
+import pandas as pf
 
 root = Tk()
 
-root.geometry("500x300")
+
+# root.geometry("300x400")
+
+def setOutput(resultaat, prob, tijd, screenName):
+    hours = int(resultaat / 60)
+    minuten = resultaat - hours * 60
+    eindtijd = str(hours + int(tijd[0:2])) + ":" + str(minuten + int(tijd[2:4]))
+
+    outScreen = Tk()
+    outScreen.title(screenName)
+    VoorspellingtxtBx = Label(outScreen, text="Voorspelling")
+    probtxtBx = Label(outScreen, text=str(round(prob, 2)) + "%", background="white", padx=10, pady=10)
+    outScreentxtBx2 = Label(outScreen, text=resultaat, background="white", padx=10, pady=10)
+    zekerheidtxt = Label(outScreen, text="Zekerheid voorspelling:", padx=10, pady=10)
+    voorspellingMinuten = Label(outScreen, text="Voorspelling in minuten:")
+    eindtijdLabel = Label(outScreen, text="Eindtijd")
+    eindtijdtxt = Label(outScreen, text=eindtijd, background="white")
+
+    voorspellingMinuten.grid(row=0, column=2, padx=10, pady=10)
+    zekerheidtxt.grid(row=0, column=3, padx=10, pady=10)
+    VoorspellingtxtBx.grid(row=1, column=1, padx=10, pady=10)
+    outScreentxtBx2.grid(row=1, column=2, padx=10, pady=10)
+    probtxtBx.grid(row=1, column=3)
+    eindtijdLabel.grid(row=2, column=1)
+    eindtijdtxt.grid(row=2, column=2)
 
 
-def data_get():
-    print("Hello")
+def fixInput(input):
+    if len(input) < 1:
+        return "00"
+    elif len(input) < 2:
+        return "0" + input
+    else:
+        return input
 
 
-def begin_click():
-    clicked = Label(root, text="Clicked.")
-    clicked.grid(row=4, column=1)
-    tree, k_means = load_joblib()
-    sap_tijd = str(invoer1_1.get()) + str(invoer1_2.get()) + "00"
-    monteur_tijd = str(invoer2_1.get()) + str(invoer2_2.get()) + "00"
-    pred = prediction([int(sap_tijd), int(monteur_tijd)], tree)
-    uitvoer.delete(0, END)
-    uitvoer.insert(0, pred[0])
+def beginClick():
+    tree, kMeans, poly, lr = loadJobLib()
+
+    sap_tijd = fixInput(str(invoer1_1.get())) + fixInput(str(invoer1_2.get())) + "00"
+    monteur_tijd = fixInput(str(invoer2_1.get())) + fixInput(str(invoer2_2.get())) + "00"
+    monteur_prognose_tijd = (str(invoer3_2.get()))
+    magie, probability = prediction([int(sap_tijd), int(monteur_tijd)], kMeans)
+    prio = int(invoer4_1.get())
+    if prio == 1:
+        lst = [1, 0, 0, 0, sap_tijd, monteur_tijd, monteur_prognose_tijd]
+    elif prio == 2:
+        lst = [0, 1, 0, 0, sap_tijd, monteur_tijd, monteur_prognose_tijd]
+    elif prio == 4:
+        lst = [0, 0, 1, 0, sap_tijd, monteur_tijd, monteur_prognose_tijd]
+    elif prio == 5:
+        lst = [0, 0, 0, 1, sap_tijd, monteur_tijd, monteur_prognose_tijd]
+    else:
+        lst = [0, 0, 0, 0, sap_tijd, monteur_tijd, monteur_prognose_tijd]
+    polyGuess = lr.predict(poly.fit_transform([lst]))[0]
+    setOutput(magie[0], probability, sap_tijd, "kNN voorspelling")
+    setOutput(int(polyGuess), probability, sap_tijd, "Polynomial voorspelling")
 
 
 # Label widgets.
@@ -33,36 +77,40 @@ uurLabel = Label(root, text="uur")
 minuutLabel = Label(root, text="minuut")
 inText1 = Label(root, text="Meldtijd")
 inText2 = Label(root, text="Aannemer Ter Plaatse")
-# inText3 = Label(root, text="Text3")
+inText3 = Label(root, text="Voorspelling Monteur")
+inText4 = Label(root, text="Prioriteitscode:")
 
 # Button widgets.
-beginButton = Button(root, text="Begin", command=begin_click)
+beginButton = Button(root, text="Begin", command=beginClick)
 
 # Input field widgets.
 invoer1_1 = Entry(root)
 invoer2_1 = Entry(root)
-# invoer3_1 = Entry(root)
+invoer3_1 = Entry(root)
 invoer1_2 = Entry(root)
 invoer2_2 = Entry(root)
-# invoer3_2 = Entry(root)
+invoer3_2 = Entry(root)
+invoer4_1 = Entry(root)
 
 # Output widget
-uitvoer = Entry(root)
+# uitvoer = Entry(root)
 
 # Grid positions
 uurLabel.grid(row=0, column=1)
 minuutLabel.grid(row=0, column=2)
 inText1.grid(row=1, column=0)
 inText2.grid(row=2, column=0)
-# inText3.grid(row=3, column=0)
+inText3.grid(row=3, column=0)
+inText4.grid(row=4, column=0)
 
 invoer1_1.grid(row=1, column=1)
 invoer2_1.grid(row=2, column=1)
 # invoer3_1.grid(row=3, column=1)
 invoer1_2.grid(row=1, column=2)
 invoer2_2.grid(row=2, column=2)
-# invoer3_2.grid(row=3, column=2)
-uitvoer.grid(row=5, column=2)
+invoer3_2.grid(row=3, column=2)
+invoer4_1.grid(row=4, column=1)
+# uitvoer.grid(row=5,column=2)
 
 beginButton.grid(row=5, column=1)
 
--- Application/dataProcessing.py
+++ Application/dataProcessing.py
@@ -1,143 +1,137 @@
 import pandas as pd
+import numpy as np
 from joblib import dump, load
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
 
 
-def clean_data():  # Specifiek script voor deze dataset, returned een dataset.
+def cleanData():  # This code is not callable from the program itself. It exists here to create the desired dataset from console.
     data = pd.read_csv("sap_storing_data_hu_project.csv")
-    storingen = pd.DataFrame(data=data, index=data.index,
-                             columns=['stm_oorz_groep', 'stm_oorz_code', 'stm_geo_gst', 'stm_fh_ddt',
-                                      'stm_sap_storeind_ddt', 'stm_sap_melddatum', 'stm_sap_meldtijd',
-                                      'stm_aanngeb_dd', 'stm_aanngeb_tijd', 'stm_aanntpl_dd',
-                                      'stm_aanntpl_tijd', 'stm_fh_dd', 'stm_fh_tijd', 'stm_fh_duur',
-                                      'stm_sap_storeinddatum', 'stm_sap_storeindtijd',
-                                      'stm_prioriteit'])
-    storingen = storingen.iloc[1:, :]
-    storingen = storingen.drop_duplicates()
-    storingen = storingen.convert_dtypes()
-    # Lege values priority 9 geven.
-    storingen['stm_prioriteit'] = storingen['stm_prioriteit'].fillna(9)
-    # Hieronder wordt een nieuwe code aangemaakt voor onbekende stm_ooz_codes, 0.
-    storingen['stm_oorz_code'] = storingen['stm_oorz_code'].astype(str)
-    storingen['stm_oorz_code'] = storingen['stm_oorz_code'].str.replace('<NA>', '0')
-    storingen['stm_oorz_code'] = storingen['stm_oorz_code'].fillna('0')
-    storingen['stm_oorz_code'] = storingen['stm_oorz_code'].astype(int)
-    # Verwijderen van 0
-    storingen = storingen[storingen.stm_fh_duur != 0]
+    disruptions = pd.DataFrame(data=data, index=data.index,
+                               columns=['stm_oorz_groep', 'stm_oorz_code', 'stm_geo_gst', 'stm_fh_ddt',
+                                        'stm_sap_storeind_ddt', 'stm_sap_melddatum', 'stm_sap_meldtijd',
+                                        'stm_aanntpl_tijd', 'stm_fh_dd', 'stm_fh_tijd', 'stm_fh_duur',
+                                        'stm_sap_storeinddatum', 'stm_sap_storeindtijd',
+                                        'stm_prioriteit', 'stm_progfh_in_duur'])
+    disruptions = disruptions.drop_duplicates()
+    disruptions = pd.DataFrame(data=disruptions, index=disruptions.index,
+                               columns=['stm_prioriteit', 'stm_aanntpl_tijd', 'stm_progfh_in_duur',
+                                        'stm_sap_meldtijd', 'stm_fh_duur'])
+    disruptions = disruptions.iloc[1:, :]
+    disruptions = disruptions.convert_dtypes()
+    disruptions['stm_prioriteit'] = disruptions['stm_prioriteit'].fillna(9)
+    disruptions['stm_prioriteit'] = disruptions['stm_prioriteit'].astype(str)
+    disruptions['stm_prioriteit'] = 'prio: ' + disruptions['stm_prioriteit']
+    dummies = pd.get_dummies(disruptions['stm_prioriteit'])
+    disruptions = disruptions.drop('stm_prioriteit', axis=1)
+    disruptions = disruptions.join(dummies)
+    disruptions = disruptions[disruptions.stm_fh_duur != 0]
+    disruptions = disruptions[disruptions.stm_fh_duur < 1440]
+    disruptions["stm_sap_meldtijd"] = disruptions["stm_sap_meldtijd"].str.replace(":", "")
+    disruptions = disruptions[disruptions.stm_sap_meldtijd != '']
+    disruptions["stm_sap_meldtijd"] = disruptions["stm_sap_meldtijd"].dropna()
+    disruptions["stm_sap_meldtijd"] = disruptions["stm_sap_meldtijd"].astype(int)
+    disruptions["stm_aanntpl_tijd"] = disruptions["stm_aanntpl_tijd"].str.replace(":", "")
+    disruptions = disruptions[disruptions.stm_aanntpl_tijd != '']
+    disruptions["stm_aanntpl_tijd"] = disruptions["stm_aanntpl_tijd"].dropna()
+    disruptions["stm_aanntpl_tijd"] = disruptions["stm_aanntpl_tijd"].astype(int)
+    disruptions = disruptions[disruptions.stm_progfh_in_duur.astype(str).apply(lambda x: not x.startswith("-"))]
+    disruptions = disruptions[disruptions.stm_progfh_in_duur.astype(str).apply(lambda x: not x.endswith("-"))]
+    disruptions = disruptions[disruptions.stm_progfh_in_duur.astype(str).apply(lambda x: x.isnumeric())]
+    disruptions['stm_progfh_in_duur'] = disruptions['stm_progfh_in_duur'].astype(int)
+    disruptions = disruptions[disruptions.stm_progfh_in_duur < 1440]
+    disruptions = disruptions[disruptions.stm_progfh_in_duur > 0]
 
-    # Outlier cleanup
-    duur = storingen['stm_fh_duur']
-    q1 = duur.quantile(0.25)
-    q3 = duur.quantile(0.75)
-    IQR = q3 - q1
-    storingen = storingen[storingen.stm_fh_duur < 3 * IQR]
-
-    # Hieronder worden alle stm_geo_gst die niet numeriek zijn, verwijderd, zodat deze hele kolom kan worden omgezet tot een int.
-    storingen = storingen[storingen.stm_geo_gst.astype(str).apply(lambda x: x.isnumeric())]
-    storingen["stm_geo_gst"] = storingen["stm_geo_gst"].astype(int)
-
-    # Hieronder worden alle tijden en data omgezet naar pure getallen, zodat het model hiermee kan werken.
-    storingen["stm_sap_melddatum"] = pd.to_datetime(storingen["stm_sap_melddatum"])
-    storingen["stm_sap_melddatum"] = storingen["stm_sap_melddatum"].dropna().apply(lambda a: int(a.strftime('%Y%m%d')))
-    storingen["stm_sap_melddatum"] = storingen["stm_sap_melddatum"].astype(int)
-
-    storingen["stm_sap_meldtijd"] = storingen["stm_sap_meldtijd"].str.replace(":", "")
-    storingen = storingen[storingen.stm_sap_meldtijd != '']
-    storingen["stm_sap_meldtijd"] = storingen["stm_sap_meldtijd"].dropna()
-    storingen["stm_sap_meldtijd"] = storingen["stm_sap_meldtijd"].astype(int)
-
-    storingen["stm_aanntpl_dd"] = storingen["stm_aanntpl_dd"].str.replace("/", "")
-    storingen = storingen[storingen.stm_aanntpl_dd != '']
-    storingen["stm_aanntpl_dd"] = storingen["stm_aanntpl_dd"].dropna()
-    storingen["stm_aanntpl_dd"] = storingen["stm_aanntpl_dd"].astype(int)
-
-    storingen["stm_aanntpl_tijd"] = storingen["stm_aanntpl_tijd"].str.replace(":", "")
-    storingen = storingen[storingen.stm_aanntpl_tijd != '']
-    storingen["stm_aanntpl_tijd"] = storingen["stm_aanntpl_tijd"].dropna()
-    storingen["stm_aanntpl_tijd"] = storingen["stm_aanntpl_tijd"].astype(int)
-
-    storingen["stm_aanngeb_dd"] = storingen["stm_aanngeb_dd"].str.replace("/", "")
-    storingen = storingen[storingen.stm_aanngeb_dd != '']
-    storingen["stm_aanngeb_dd"] = storingen["stm_aanngeb_dd"].dropna()
-    storingen["stm_aanngeb_dd"] = storingen["stm_aanngeb_dd"].astype(int)
-
-    storingen["stm_aanngeb_tijd"] = storingen["stm_aanngeb_tijd"].str.replace(":", "")
-    storingen = storingen[storingen.stm_aanngeb_tijd != '']
-    storingen["stm_aanngeb_tijd"] = storingen["stm_aanngeb_tijd"].dropna()
-    storingen["stm_aanngeb_tijd"] = storingen["stm_aanngeb_tijd"].astype(int)
-
-    # Hieronder worden dummies aangemaakt voor stm_oporzaak_groep.
-    dummies = pd.get_dummies(storingen['stm_oorz_groep'])
-    storingen = storingen.drop('stm_oorz_groep', axis=1)
-    storingen = storingen.join(dummies)
-    return storingen
+    return disruptions
 
 
-def write_csv(data, name):
-    data.to_csv(path_or_buf=name, index=False, mode='w', sep=';')
+def writeCSV(data, csvName):  # Writing a new .csv file with designate data and name.
+    data.to_csv(path_or_buf=csvName, index=False, mode='w', sep=';')
 
 
-def train_test(storingen):
+def trainTest(storingen):
     # Allereerst moeten de x en de y worden bepaald.
     x = storingen[['stm_sap_meldtijd', 'stm_aanntpl_tijd']]
     y = storingen['stm_fh_duur']
     y = y.astype('int')
+
     # Daarna worden deze opgesplitst in een train en een test set.
-    x_train, x_test, y_train, y_test = train_test_split(x, y)
-    return x_train, x_test, y_train, y_test
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y)
+    return X_train, X_test, Y_train, Y_test
 
 
-def decision_tree(depth, x_train, x_test, y_train, y_test):
+def decisionTree(depth, X_train, X_test, Y_train, Y_test):
     # Nu we onze data goed hebben voorbereid, kunnen er modellen mee getraind worden.
     # Allereerst trainen we een DecisionTreeClassifier, en bepalen we de score.
     boom = DecisionTreeClassifier(max_depth=depth)
-    boom.fit(x_train, y_train)
-    acc = boom.score(x_test, y_test)
+    boom.fit(X_train, Y_train)
+    acc = boom.score(X_test, Y_test)
+    # print("Acc: " + str(acc))
+
+    # Hieronder staat het script dat de beste waarde voor de max depth opzoekt. We zijn hier geintereseerd in de waarde waar de curve afvlakt.
+
     return acc, boom
 
 
-def k_means(neighbours, x_train, x_test, y_train, y_test):
+def kMeans(neighbours, X_train, X_test, Y_train, Y_test):
+    # Nu trainen we ook een KNN model. Dit doen we ten eerste om te kijken welk model beter scoort, maar ook als een controle of ons eerste model niet overfit is.
     knn = KNeighborsClassifier(n_neighbors=neighbours)
-    knn.fit(x_train, y_train)
+    knn.fit(X_train, Y_train)
     knn.set_params(weights="distance")
-    acc = knn.score(x_test, y_test)
+    acc = knn.score(X_test, Y_test)
+    # print("Model Accuracy: " + str(acc))
     return acc, knn
 
 
-def prediction(targets, classifier):
-    pred = classifier.predict([targets])
-    return pred
+def polynomial():
+    X = storingen[
+        ['prio: 1', 'prio: 2', 'prio: 4', 'prio: 5', 'stm_sap_meldtijd', 'stm_aanntpl_tijd', 'stm_progfh_in_duur']]
+    Y = storingen['stm_fh_duur'].astype('int')
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+    pf = PolynomialFeatures(degree=3)
+    pf_ft = pf.fit_transform(X_test)
+    pf.fit(pf_ft, Y_test)
+    l_r = LinearRegression()
+    l_r.fit(pf_ft, Y_test)
+    acc = l_r.score(pf_ft, Y_test)
+
+    return acc, pf, l_r
 
 
-def write_joblib(tree, kmeans, x_test, y_test):
+def prediction(lst, classifier):
+    prediction = classifier.predict([lst])
+    probability = classifier.predict_proba([lst])[-1]
+    probability = probability.tolist()
+    probability.sort()
+    return prediction, probability[-1] * 100
+
+
+def writeJobLib(tree, kMeans, poly, lr):
     dump(tree, 'tree.joblib')
-    dump(kmeans, 'kMeans.joblib')
-    dump(x_test, 'x_test.joblib')
-    dump(y_test, 'y_test.joblib')
+    dump(kMeans, 'kMeans.joblib')
+    dump(poly, 'poly.joblib')
+    dump(lr, 'lr.joblib')
 
-def load_joblib():
+
+def loadJobLib():
     tree = load('tree.joblib')
-    kmeans = load('kMeans.joblib')
-    x_test = load('x_test.joblib')
-    y_test = load('y_test.joblib')
-    return tree, kmeans, x_test, y_test
+    kMeans = load('kMeans.joblib')
+    poly = load('poly.joblib')
+    lr = load('lr.joblib')
+    return tree, kMeans, poly, lr
 
 
-# write_csv(clean_data(), 'cleaned.csv')
-
-#storingen = pd.read_csv("cleaned.csv", sep=';')
-#x_train, x_test, y_train, y_test = train_test(storingen)
-#treeAcc, tree = decision_tree(52, x_train, x_test, y_train, y_test)
-#kMeansAcc, kmeans = k_means(55, x_train, x_test, y_train, y_test)
-# # print(treeAcc, kMeansAcc)
-# # #
-#print('writing...')
-#write_joblib(tree, kmeans, x_test, y_test)
+# data = cleanData()
+# writeCSV(data, 'cleaned.csv')
 #
-tree, kmeans, x_test, y_test = load_joblib()
+# storingen = pd.read_csv("cleaned.csv", sep=';')
+# X_train, X_test, Y_train, Y_test = trainTest(storingen)
+# treeAcc, tree = decisionTree(52, X_train, X_test, Y_train, Y_test)
+# kMeansAcc, kMeans = kMeans(55, X_train, X_test, Y_train, Y_test)
+# polyAcc, poly, lr = polynomial()
 #
-print(tree.score(x_test, y_test))
-print(kmeans.score(x_test, y_test))
-
+# writeJobLib(tree, kMeans, poly, lr)
